5d6529412087304a8e48bc56c7760301d828c422
Tomislav Koretic <opel.tigra16@gmail.com>
2019-03-25 19:50:06 +0100

    Add Task4,5,6

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var vri= newDate ()
 var halls = ['dvorana1', 'dvorana2', 'dvorana3', 'dvoran4', 'dvorana5'];
 var dvorana1 = {
 id:1,
@@ -50,15 +51,50 @@ var dvorana2 = {
                 }
 
 
-   }
    
    
-   console.log(halls);
+   
+   
 
    //nadam se da je to okej 
 
 
 
-const server = require('server');
+//var server = require('server');
 
-server(ctx => 'Hello world!');
+//server(ctx => 'Hello world!');
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+//task 4
+halls.push(dvorana1, dvorana2, dvorana3, dvorana4, dvorana5);
+var imenaDvorana = [];
+imenaDvorana.push(dvorana1.name, dvorana2.name, dvorana3.name, dvorana4.name, dvorana5.name);
+console.log(imenaDvorana);
+
+//task 5
+var imenaDvoranaString = imenaDvorana.toString();
+console.log(imenaDvoranaString);
+
+//task 6
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end(imenaDvoranaString);
+}).listen(8080);
+
+console.log('Server started');
