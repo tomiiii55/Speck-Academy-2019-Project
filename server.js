8eb0cd10c3e1576845cf7b66954c70e60d3c841a
Tomislav Koretic <opel.tigra16@gmail.com>
2019-04-10 22:15:08 +0200

    New server.js

--- server.js
+++ server.js
@@ -1,8 +1,50 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {
+    halls,
+    init: initHalls,
+    add,
+    update,
+    remove
+} = require('./halls');
+
+initHalls();
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
+app.use("/", express.static('public'));
+
+app.get("/halls", function (req, res) {
+    res.json(halls);
 });
 
-app.listen(3015, () => console.log('Gator app listening on port 3015!'));
+app.post("/hallsCreate", function (req, res) {
+    add(req.body.hallName);
+    res.redirect("/halls");
+});
+
+app.post("/hallsUpdate", function (req, res) {
+    const hallId = parseInt(req.body.hallId);
+    const isReserved = req.body.isReserved === 'on';
+    const reservedFrom = new Date();
+    const reservedUntil = new Date(req.body.reservedUntil);
+
+    update(hallId, isReserved, reservedFrom, reservedUntil);
+
+    res.redirect("/halls");
+});
+
+app.post("/hallsDelete", function (req, res) {
+    const hallId = parseInt(req.body.hallId);
+    remove(hallId);
+    res.redirect("/");
+});
+
+app.listen(3015);
